49ab0a2711fe6a75d311d1c8ccce144749d95302
viktor <viktor@enapps.co.uk>
2016-01-20 13:45:31 +0200

    First commit

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested - ' + form.login.data)
+        return redirect('/index')
+    return render_template('login.html',
+                            title = 'Sigh In',
+                            form = form,
+                            providers = app.config['OPENID_PROVIDERS'])
---
+++ config.py
@@ -0,0 +1,9 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'admin123'
+OPENID_PROVIDERS = [
+    {'name': 'viktor', 'password': 'viktor'},
+    # {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    # {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    # {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    # {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
