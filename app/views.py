1f0a53e458b93dfcd156fc89beda84940ff2abbd
viktor <viktor.pecheniuk@gmail.com>
2016-05-07 20:13:55 +0300

    refactored

--- app/views.py
+++ app/views.py
@@ -3,24 +3,19 @@ from app import app
 from forms import LoginForm
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested - ' + form.login.data)
+        return render_template('admin.html', title='People')
+    return render_template('index.html',
+                           title='Sigh In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS']
+                           )
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested - ' + form.login.data)
-        return render_template('admin.html', title='People')
-    return render_template('login.html',
-                           title='Sigh In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS']
-                           )
