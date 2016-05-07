d876d01ad8b994e4ce287f1152f6308fcfa3e795
viktor <viktor.pecheniuk@gmail.com>
2016-05-07 18:57:54 +0300

    refactored

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, PasswordField, SubmitField
+from wtforms import TextField, PasswordField, SubmitField
 from wtforms.validators import Required
 
 
 class LoginForm(Form):
     login = TextField('Login', validators=[Required()])
     password = PasswordField('Password', validators=[Required()])
-    # remember_me = BooleanField('remember_me', default=False)
     submit = SubmitField("Send")
